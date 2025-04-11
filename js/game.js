5f88eda185fead8a8107c541ccfdae73a9ad08f0
Joyce-Nkengbeza <Joyce.nkengbeza@stu.ocsb.ca>
2025-04-11 17:14:38 +0000

    centered

--- js/game.js
+++ js/game.js
@@ -4,8 +4,11 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
-} 
-
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scle.CENTER_BOTH,
+  },
+}
 const game = new Phaser.Game(config)
 console.log(game)
