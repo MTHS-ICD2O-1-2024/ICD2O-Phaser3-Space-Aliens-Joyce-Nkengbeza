0315b0e93d5b58108aff2544193d5750d9370a79
Joyce-Nkengbeza <Joyce.nkengbeza@stu.ocsb.ca>
2025-04-19 02:36:41 +0000

    added splash scene

--- js/game.js
+++ js/game.js
@@ -1,5 +1,17 @@
 /*global Phaser*/
 
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Joyce Nkengbeza
+// Created on: April 2025
+// This file contains the JS functions for index.html
+
+import SplashScene from './splashScene.js'
+
+//Our game scene
+const splashScene = new SplashScene()
+
+//*Game scene*/
 const config = {
   type: Phaser.AUTO,
   width: 1920,
@@ -10,5 +22,12 @@ const config = {
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
 }
+
 const game = new Phaser.Game(config)
-console.log(game)
+
+//load scenes
+//NOTE: remember any "key" is global and CAN NOT be reused!
+game.scene.add('splashScene', splashScene)
+
+//start title
+game.scene.start('splashScene')
