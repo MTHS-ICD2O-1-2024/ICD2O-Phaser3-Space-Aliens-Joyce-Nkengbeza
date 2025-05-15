7a1456a15ee80fd5e9a736ca1f03e9eb566981b8
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-05-15 16:56:47 +0000

    added

--- js/gameScene.js
+++ js/gameScene.js
@@ -1,8 +1,6 @@
 /*global Phaser*/
 
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-
-//
+// Copyright (c) 2020 Mr. Coxall All rights reserved //
 
 // Created by: Joyce Nkengbeza
 
@@ -47,7 +45,29 @@ class GameScene extends Phaser.Scene {
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
   }
 
-  update(time, delta) {}
+  update(time, delta) {
+    //called 60 times a second, hopefully!
+
+    const keyLeftObj = this.input.keyboard.addKey("LEFT")
+
+    const keyRightObj = this.input.keyboard.addKey("RIGHT")
+
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 15
+
+      if (this.ship.x < 0) {
+        this.ship.x = 0
+      }
+    }
+
+    if (keyRightObj.isDown === true) {
+      this.ship.x += 15
+
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
+      }
+    }
+  }
 }
 
 export default GameScene
