027144a95ce70941a3389dbb0f479d5fddd20aed
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-06-05 16:35:41 +0000

    gamejs

--- js/game.js
+++ js/game.js
@@ -30,18 +30,25 @@ const gameScene = new GameScene()
 
 const config = {
   type: Phaser.AUTO,
+
   width: 1920,
+
   height: 1080,
-  backgroundColor: 0xffffff,
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-  },
+
   physics: {
     default: "arcade",
+
     arcade: {
       debug: false,
     },
+  },
+
+  backgroundColor: 0xffffff,
+
+  scale: {
+    mode: Phaser.Scale.FIT,
+
+    autoCenter: Phaser.Scale.CENTER_BOTH,
   },
 }
 
